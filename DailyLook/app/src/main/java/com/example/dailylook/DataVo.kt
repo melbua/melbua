d930ffe751db222ae40523376acdcea51f072d79
hyunho <melbua@naver.com>
2022-11-30 13:51:19 +0900

    파일 삭제

--- DailyLook/app/src/main/java/com/example/dailylook/DataVo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dailylook
-
-import android.net.Uri
-
-
-//class DataVo(val name: String?, val id:String?, val photo:String)
-
-class DataVo(val name: String?, val id: String, val address: String?, val pay: Int?, val photo: Uri?)
